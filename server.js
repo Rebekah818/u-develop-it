4c54fb3ef3fb20956262d43a8228c392e5eeedc8
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-02-08 19:09:52 -0500

    array of objects added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,9 +8,34 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+
+
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        // your mysql username,
+        user: 'root',
+        // Your mysql password 
+        password: 'Isabelle312*',
+        database: 'election'
+    },
+    console.log('connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
+});
+
+app.get(`/`, (req, res) => {
+    res.json({
+        message: 'hello world'
+    });
 });
 
 app.listen(PORT, () => {
